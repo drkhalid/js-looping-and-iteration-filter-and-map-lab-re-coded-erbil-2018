42a541db04572be23bb88135699e177349474f86
khalid jamal <kh.jamal95@gmail.com>
2018-08-12 19:47:39 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,11 +4,5 @@ function driversWithRevenueOver(list,reven){
 }
 
 function driverNamesWithRevenueOver(list,reven){
-  return list.map(function(driver){
-    if (driver.revenue > reven){
-      let array =[]
-      array.push[`${driver.name}`]
-      return array
-    }
-  })
+  
 }
